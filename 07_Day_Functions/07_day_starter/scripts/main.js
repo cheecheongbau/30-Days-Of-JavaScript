aad02a0385f37b02e2cb34da192f98f372d18d2c
cheecheongbau <44594865+cheecheongbau@users.noreply.github.com>
2022-07-14 12:11:07 +0800

    d7 d8

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,92 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//console.log(countries)
+//alert('Open the console and check if the countries has been loaded')
+/*### Exercises: Level 1
+
+1. Declare a function _fullName_ and it print out your full name.
+2. Declare a function _fullName_ and now it takes firstName, lastName as a 
+parameter and it returns your full - name.
+3. Declare a function _addNumbers_ and it takes two two parameters and 
+it returns sum.
+*/
+function fullName1() {
+    console.log(`Full name`)
+}
+fullName1()
+
+function fullName2(firstName, lastName) {
+    console.log(`${firstName} ${lastName}`)
+}
+fullName2('Bau', 'Bee')
+
+function addNumbers(param1, param2) {
+    return param1 + param2
+}
+console.log(`Add Numbers: ${addNumbers(4,5)}`)
+/*
+4. An area of a rectangle is calculated as follows: 
+_area = length x width_. Write a function which calculates _areaOfRectangle_.
+5. A perimeter of a rectangle is calculated as follows: 
+_perimeter= 2x(length + width)_. Write a function which calculates 
+_perimeterOfRectangle_.
+6. A volume of a rectangular prism is calculated as follows: 
+_volume = length x width x height_. Write a function which calculates 
+_volumeOfRectPrism_.
+*/
+function areaOfRectangle(length, width){
+    return length * width
+}
+console.log(`Area of Rectangle: ${areaOfRectangle(5, 20)}`)
+
+function perimeterOfRectangle(length, width) {
+    return 2* (length + width)
+}
+console.log(`Perimeter of Rectangle: ${perimeterOfRectangle(5, 20)}`)
+
+function volOfRectPrism(len, width, height) {
+    return len * width * height
+}
+console.log(`Vol of Rect: ${volOfRectPrism(5, 20, 5)}`)
+/*
+7. Area of a circle is calculated as follows: 
+_area = π x r x r_. Write a function which calculates _areaOfCircle_
+8. Circumference of a circle is calculated as follows: 
+_circumference = 2πr_. Write a function which calculates _circumOfCircle_
+*/
+function areaOfCircle(radius) {
+    return Math.PI * radius * radius
+}
+console.log(`Area of Circle: ${areaOfCircle(5)}`)
+/*
+9. Density of a substance is calculated as follows:
+_density= mass/volume_. Write a function which calculates _density_.
+10. Speed is calculated by dividing the total distance covered by 
+a moving object divided by the total amount of time taken. 
+Write a function which calculates a speed of a moving object, _speed_.
+11. Weight of a substance is calculated as follows: 
+_weight = mass x gravity_. Write a function which calculates _weight_.
+12. Temperature in oC can be converted to oF using this formula: 
+_oF = (oC x 9/5) + 32_. Write a function which convert oC to oF 
+_convertCelsiusToFahrenheit_.
+13. Body mass index(BMI) is calculated as follows: 
+_bmi = weight in Kg / (height x height) in m2_. 
+Write a function which calculates _bmi_. 
+BMI is used to broadly define different weight groups in 
+adults 20 years old or older.Check if a person is 
+_underweight, normal, overweight_ or _obese_ based the information given below.
+
+    - The same groups apply to both men and women.
+    - _Underweight_: BMI is less than 18.5
+    - _Normal weight_: BMI is 18.5 to 24.9
+    - _Overweight_: BMI is 25 to 29.9
+    - _Obese_: BMI is 30 or more
+
+14. Write a function called _checkSeason_, it takes a month parameter and returns the season:Autumn, Winter, Spring or Summer.
+15. Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
+
+    ```js
+    console.log(findMax(0, 10, 5))
+    10
+    console.log(findMax(0, -10, -2))
+    0
+    ```
+*/
--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -1,2 +1,32 @@
 console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+/*
+### Exercises: Level 1
+
+1. Create an empty object called dog
+1. Print the the dog object on the console
+1. Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return _woof woof_
+1. Get name, legs, color, age and bark value from the dog object
+1. Set new properties the dog object: breed, getDogInfo
+*/
+const dog = {}
+console.log(dog)
+dog.name = "bobby"
+dog.legs = 4
+dog.color = "silver"
+dog.age = 10 
+dog.bark = function() {
+    return "woof woof"
+}
+console.log(dog.name)
+console.log(dog.legs)
+console.log(dog.color)
+console.log(dog.age)
+console.log(dog.bark())
+
+dog.breed = "retriever"
+dog.getDogInfo = function() {
+    return this
+}
+console.log(dog.breed)
+console.log(dog.getDogInfo())
